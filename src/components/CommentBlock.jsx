6dfaf1baeb8ad3ae44c2f1641e2bc4cfcc9c68f8
PhanQuocHuyIUH <22714321.huy@student.iuh.edu.vn>
2025-03-20 13:10:55 +0700

    update detail page: reviews product

--- src/components/CommentBlock.jsx
+++ src/components/CommentBlock.jsx
@@ -10,15 +10,10 @@ const CommentBlock = ({ userName, date, comment }) => {
         className="h-8 w-8 mr-5"
       />
       <div className="flex flex-col">
-        <h1 className="mb-2 text-sm font-normal">Phan Huy</h1>
+        <h1 className="mb-2 text-sm font-normal">{userName}</h1>
         <StarReview star={5} className={"mb-2"} />
-        <h2 className="text-xs font-light">2025-1-30 10:42</h2>
-        <p className="text-base font-normal mt-4 w-[80%]">
-          Sản phẩm siu chất lượng lunnnnn. Không khác gì nhiều so với tay cầm
-          của hãng S. Ngoài ra còn 1 số chức năng khác như turbo,... Shop chăm
-          sóc khách hàng siu siu đỉnh lun nhé, trò chuyện hay hỏi về sản phẩm
-          đều oge lun. Nên mua nheéee
-        </p>
+        <h2 className="text-xs font-light">{date}</h2>
+        <p className="text-base font-normal mt-4 w-[80%]">{comment}</p>
       </div>
     </div>
   );
